ca6ab12367bea280ef007084c2dd10a4f1960782
Mikołaj Radwan <mikolaj.radwan@techland.pl>
2017-03-20 16:21:09 +0100

    Prepare for 0.1 release

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ import setuptools
 distutils.core.setup(
     name='cppbuildprofiler',
     version='0.1',
-    description='A tool that attempts to facilitate profiling C++ builds.',
+    description='A tool that facilitates profiling C++ builds.',
     author='Mikolaj Radwan @ Techland',
     author_email='mikolaj.radwan@techland.pl',
     url='https://github.com/techland-games/cpp-build-profiler',
+    download_url='https://github.com/techland-games/cpp-build-profiler/archive/0.1.tar.gz',
     license='MIT',
     packages=['cppbuildprofiler'],
+    keywords=['c++', 'building', 'compilation', 'profiling', 'optimisation'],
     entry_points={
         'console_scripts': [
             'cppbuildprofiler-cli = cppbuildprofiler.cli:main',
