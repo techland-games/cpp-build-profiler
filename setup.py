0dec101a4e0148a37809029d99c1ebc16622f2c7
Mikołaj Radwan <mikolaj.radwan@techland.pl>
2017-05-02 13:54:14 +0200

    Prepare for 1.0 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 
 distutils.core.setup(
     name='cppbuildprofiler',
-    version='0.4',
+    version='1.0',
     description='A tool that facilitates profiling C++ builds.',
     author='Mikolaj Radwan @ Techland',
     author_email='mikolaj.radwan@techland.pl',
